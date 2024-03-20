10fad11f492b98d5ae7cc940524fb3855361360b
zzx135790 <2661675652@qq.com>
2024-03-20 14:23:24 +0800

    single model test

--- detect/common.py
+++ detect/common.py
@@ -29,22 +29,25 @@ num_detect = 6
 #     0: [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
 # }
 
-# # model_config路径，目前是co-detr会使用
-# config_path = [
-#     '',
-#     # '/mnt/workspace/Co-detr/work_dirs/swin-b/gc/swinb-GC.py',
-#     # "/mnt/workspace/Co-detr/work_dirs/codetr_r50_size/codetr_r101_size.py",
-#     # "/mnt/workspace/Co-detr/work_dirs/swin-b/deroi/swinb-deroi.py"
-# ]
+# model_config路径，目前是co-detr会使用
+config_path = [
+    '',
+    '',
+    '',
+    ''
+    # '/mnt/workspace/Co-detr/work_dirs/swin-b/gc/swinb-GC.py',
+    # "/mnt/workspace/Co-detr/work_dirs/codetr_r50_size/codetr_r101_size.py",
+    # "/mnt/workspace/Co-detr/work_dirs/swin-b/deroi/swinb-deroi.py"
+]
 
-# # model路径
-# models_path = [
-#     # "/mnt/workspace/Co-detr/work_dirs/yolo/yolov8n.pt",
-#     "C:/Users/zzx123/Desktop/work/竞赛/服务外包/模型/小权重/best.pt",
-#     # "/mnt/workspace/Co-detr/work_dirs/swin-b/gc/best_bbox_mAP_epoch_3.pth",
-#     # "/mnt/workspace/Co-detr/work_dirs/codetr_r50_size/best_bbox_mAP_epoch_12.pth",
-#     # "/mnt/workspace/Co-detr/work_dirs/swin-b/deroi/best_bbox_mAP_epoch_4.pth"
-# ]
+# model路径
+models_path = [
+    "C:/Users/zzx123/Desktop/work/竞赛/服务外包/模型/大权重/best.pt",
+    "C:/Users/zzx123/Desktop/work/竞赛/服务外包/模型/小权重/best.pt",
+    "C:/Users/zzx123/Desktop/work/竞赛/服务外包/模型/大权重dropout0.1/best.pt",
+    "C:/Users/zzx123/Desktop/work/竞赛/服务外包/模型/大权重dropout0.2/0.2.pt",
+    # "/mnt/workspace/Co-detr/work_dirs/swin-b/deroi/best_bbox_mAP_epoch_4.pth"
+]
 
 # 权重字典，需要自定义
 weight_dict = {
@@ -53,22 +56,22 @@ weight_dict = {
     2: [0.25698695992452675, 0.2652949616001609, 0.23601820539956264, 0.2461956380068944, 0.2431197782282009, 0.25],
     3: [0.26454530666753523, 0.2798033169454364, 0.23978518437376897, 0.24439178185772814, 0.2840433376414377, 0.25]
 }
-
-# model_config路径，目前是co-detr会使用
-config_path = [
-    '',
-    '/mnt/workspace/Co-detr/work_dirs/swin-b/gc/swinb-GC.py',
-    "/mnt/workspace/Co-detr/work_dirs/codetr_r50_size/codetr_r101_size.py",
-    "/mnt/workspace/Co-detr/work_dirs/swin-b/deroi/swinb-deroi.py"
-]
-
-# model路径
-models_path = [
-    "/mnt/workspace/Co-detr/work_dirs/yolo/yolov8n.pt",
-    "/mnt/workspace/Co-detr/work_dirs/swin-b/gc/best_bbox_mAP_epoch_3.pth",
-    "/mnt/workspace/Co-detr/work_dirs/codetr_r50_size/best_bbox_mAP_epoch_12.pth",
-    "/mnt/workspace/Co-detr/work_dirs/swin-b/deroi/best_bbox_mAP_epoch_4.pth"
-]
+#
+# # model_config路径，目前是co-detr会使用
+# config_path = [
+#     '',
+#     '/mnt/workspace/Co-detr/work_dirs/swin-b/gc/swinb-GC.py',
+#     "/mnt/workspace/Co-detr/work_dirs/codetr_r50_size/codetr_r101_size.py",
+#     "/mnt/workspace/Co-detr/work_dirs/swin-b/deroi/swinb-deroi.py"
+# ]
+#
+# # model路径
+# models_path = [
+#     "/mnt/workspace/Co-detr/work_dirs/yolo/yolov8n.pt",
+#     "/mnt/workspace/Co-detr/work_dirs/swin-b/gc/best_bbox_mAP_epoch_3.pth",
+#     "/mnt/workspace/Co-detr/work_dirs/codetr_r50_size/best_bbox_mAP_epoch_12.pth",
+#     "/mnt/workspace/Co-detr/work_dirs/swin-b/deroi/best_bbox_mAP_epoch_4.pth"
+# ]
 
 # 输出文件的路径
 output_file = "sf.txt"
